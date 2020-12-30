8480a34c0ad5f0ca354991736de5cd9723880665
PN\nhang.dang21 <nhang.dang21@student.passerellesnumeriques.org>
2020-12-30 11:23:12 +0700

    Add folder show_img that show img by click button

---
+++ js/object/index.js
@@ -0,0 +1,6 @@
+var person = {
+    name: 'Nhang',
+    age: 20,
+    gender: 'male',
+    add: 'danang'
+}
---
+++ js/show_img/index.js
@@ -0,0 +1,7 @@
+function displayapple() {
+    document.getElementById('img_fruit').src = 'img/apple.jfif';
+}
+
+function displaymango() {
+    document.getElementById('img_fruit').src = 'img/mango.webp';
+}
