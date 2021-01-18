e89de0f8782727e0ff0b3f54f19069b62e3c91eb
dangvannhang <dangvannhang107@gmail.com>
2021-01-18 10:45:07 +0700

    Finish todolist with add task.

--- framework/vue/my-vue/src/App.vue
+++ framework/vue/my-vue/src/App.vue
@@ -2,47 +2,16 @@
   <div id="app">
 
 
-    <input type="text" v-model="newTask">
-
-    <button @click="tasks.push({content: newTask, done: false})">Them</button>
-
-    <div v-for="(task,index) in tasks" :key="index">
-      <input type="checkbox" v-model="task.done" >
-      <span :class="{isFinished: task.done} ">
-        {{task.content}}
-      </span>
-    </div>
-
 
   </div>
 </template>
 <script>
 
 export default {
-  data() {
-    return {
-      newTask:'',
-      tasks:[
-        {
-          content:'watching films',
-          done:false
-          },
-        {
-          content:'Reading books',
-          done:false
-          },
-        {
-          content:'Playing game',
-          done:false
-          },
-      ]
-    }
-  },
+ 
 
 }
 </script>
 <style>
-.isFinished{
-  text-decoration-line: line-through;
-}
+
 </style>
